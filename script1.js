e01a5b3fc0944b2dc9098c7ffd6da9b4ee012a9c
Davit <90.davitmanukyan@mail.ru>
2021-12-02 21:05:12 +0400

    Print “odd” and evem

---
+++ script1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let arr = [125, 35, 20];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    console.log(arr[i] + " even");
+  } else {
+    console.log(arr[i] + " odd");
+  }
+}
+
+
+
+
+// let result = function (e) {
+//   if (e % 2 === 0) {
+//     console.log(e + " even");
+//   } else {
+//     console.log(e + " odd");
+//   }
+// };
+
+// result(125);
+// result(35);
+// result(20);
