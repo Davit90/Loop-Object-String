b6cae98cc5c403e13a5f19caf1808809a90267ed
Davit <90.davitmanukyan@mail.ru>
2021-12-02 21:00:43 +0400

    with while and do-while

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let i = 0;
+let arr = ["Davit", "Manukyan"];
+
+// while (i < arr.length) {
+//   console.log("for", i, arr[i]);
+//   i++;
+// }
+
+
+do {
+  console.log("for", i, arr[i]);
+  i++;
+} while (i < arr.length);
